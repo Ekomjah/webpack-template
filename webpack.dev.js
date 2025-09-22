38ed2b9f6a648c46b41c0025c8ee8ecac9fc9f2f
Ekomjah Denis <ekomjahedet@gmail.com>
2025-09-22 04:32:23 +0100

    Ensure that the whole repo works well

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,14 @@
 const common = require("./webpack.common.js");
 const { merge } = require("webpack-merge");
+const path = require("path");
+
 module.exports = merge(common, {
   mode: "development",
   devtool: "eval-source-map",
   devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"), // serve from dist
+    },
     watchFiles: ["./src/template.html"],
   },
 });
